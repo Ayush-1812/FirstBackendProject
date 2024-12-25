e9cfc135a503b1bfa67a4fc5e7e4797824b9d47b
Ayush-1812 <2023kucp1053@iiitkota.ac.in>
2024-12-25 23:15:39 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -8,3 +8,12 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 11000,()=>{
+    console.log(`Server is running at port :${process.env.PORT}`)
+  })
+})
+.catch((err)=>{
+  console.log("Mongo db connection failed !! ",err);
+  
+})
